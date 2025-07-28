792ed74c507b14ef34f5056079a8c2323e13fec3
Wesley Werikis <wesleywerikis@outlook.com>
2025-07-28 14:17:18 -0300

    commit inicial - projeto Spring Boot

---
+++ src/main/java/br/com/unitryn/app_unitryn/AppUnitrynApplication.java
@@ -0,0 +1,13 @@
+package br.com.unitryn.app_unitryn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUnitrynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUnitrynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unitryn/app_unitryn/AppUnitrynApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unitryn.app_unitryn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUnitrynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
